86f6563e5022b9ec02a1a901fcba0a81217be31d
heathenthrone48 <ashinsrajgad@gmail.com>
2021-09-16 23:20:02 +0530

    Inistial Commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div class="container">
+        <div class="fl flex1">
+          Flex 1
+        </div>
+        <div class="fl flex2">
+          Flex 2
+        </div>
+        <div class="fl flex3">
+          Flex 3
+        </div>
+        <div class="fl flex4">
+          Flex 4
+        </div>
+        <div class="fl flex5">
+          Flex 5
+        </div>
+        <div class="fl flex6">
+          Flex 6
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
