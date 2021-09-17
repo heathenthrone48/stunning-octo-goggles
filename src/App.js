c7ee1ef2b6c37974bf456f356936c3f538bf474e
heathenthrone48 <ashinsrajgad@gmail.com>
2021-09-17 10:14:18 +0530

    change

--- src/App.js
+++ src/App.js
@@ -1,31 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <div class="container">
-        <div class="fl flex1">
-          Flex 1
-        </div>
-        <div class="fl flex2">
-          Flex 2
-        </div>
-        <div class="fl flex3">
-          Flex 3
-        </div>
-        <div class="fl flex4">
-          Flex 4
-        </div>
-        <div class="fl flex5">
-          Flex 5
-        </div>
-        <div class="fl flex6">
-          Flex 6
+class App extends Component{
+  render(){
+    return (
+      <div className="App">
+        <div class="container">
+          <div class="fl flex1">
+            Flex 1
+          </div>
+          <div class="fl flex2">
+            Flex 2
+          </div>
+          <div class="fl flex3">
+            Flex 3
+          </div>
+          <div class="fl flex4">
+            Flex 4
+          </div>
+          <div class="fl flex5">
+            Flex 5
+          </div>
+          <div class="fl flex6">
+            Flex 6
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
